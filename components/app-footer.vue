fee184bab334ddb5f8350fc85527ef03f240625b
Xillians <sandra.toftemo@bjerk.io>
2025-03-04 19:20:00 +0100

    add header and footer

---
+++ components/app-footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <p>
+      Created by
+      <a href="https://github.com/Xillians">Xillians</a>
+    </p>
+    <a href="https://github.com/Xillians/paint-collection">
+      <img 
+        height="32"
+        src="https://cdn-icons-png.flaticon.com/512/25/25231.png" 
+        alt="GitHub" 
+      />
+    </a>
+  </footer>
+</template>
+
+<style>
+footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+}
+</style>
---
+++ components/app-header.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+ const auth = useAuthState();
+
+ async function logOut() {
+  const router = useRouter();
+  try {
+    await $fetch('/api/logout', {
+      method: 'GET',
+    });
+    auth.value.isLoggedIn = false;
+    router.push('/login');
+  } catch (error) {
+    console.error('Error logging out', error);
+  }
+ }
+</script>
+<template>
+  <header>
+    <h1>
+      Paint collection
+    </h1>
+    <button v-if="auth.isLoggedIn" @click="logOut">Log out</button>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+}
+</style>
