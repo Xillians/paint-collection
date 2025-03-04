e2d2366b0920bf1aa4a18c200ba920676d65fd58
Xillians <sandra.toftemo@bjerk.io>
2025-03-04 22:20:53 +0100

    handle max age for JWT

--- server/api/login.ts
+++ server/api/login.ts
@@ -1,5 +1,8 @@
 import { readBody } from 'h3';
 import { setCookie } from 'h3';
+import paintApi from '~/composables/paintApi';
+import { ErrorModel } from '../utils/openapi';
+import { differenceInSeconds, parseISO } from 'date-fns';
 
 type LoginBody = {
   username: string;
@@ -9,21 +12,51 @@ type LoginBody = {
 export default defineEventHandler(async (event) => {
   const body = await readBody<LoginBody>(event);
   console.log(JSON.stringify(body));
-  const userId = 4201;
 
-  setCookie(event, 'session', userId.toString(), {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'strict',
-    path: '/',
-    maxAge: 60 * 60 * 24 * 1 // 1 day
-  });
+  try {
+    const userId = 4201;
+    const response = await paintApi.users.getLogin(userId);
+    if (!('token' in response)) {
+      response satisfies ErrorModel;
+      throw createError({
+        status: response.status,
+        message: response.detail
+      })
+    }
 
-  // set a cookie for client to read
-  setCookie(event, 'clientSession', userId.toString(), {
-    httpOnly: false,
-    secure: false,
-    sameSite: 'strict',
-    maxAge: 60 * 60 * 24 * 1 // 1 day
-  });
+    // A hack with date-fns and non-standard date format
+    const expiresAtString = response.expires_at.split(' ')[0] + 'T' + response.expires_at.split(' ')[1];
+
+    const expiresAt = parseISO(expiresAtString);
+    const now = new Date();
+    const maxAge = differenceInSeconds(expiresAt, now);
+
+    setCookie(event, 'session', response.token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: 'strict',
+      path: '/',
+      maxAge: maxAge
+    });
+
+    // set a cookie for client to read
+    setCookie(event, 'clientSession', response.token, {
+      httpOnly: false,
+      secure: false,
+      sameSite: 'strict',
+      maxAge: maxAge
+    });
+  } catch (error: any) {
+    if ("body" in error) {
+      const err = error.body as ErrorModel;
+      throw createError({
+        status: err.status,
+        message: err.detail
+      });
+    }
+    throw createError({
+      status: 500,
+      message: 'Internal Server Error'
+    });
+  }
 });
