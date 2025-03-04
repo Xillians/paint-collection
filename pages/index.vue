78b9c2eb27b7d7f60ab0fc2bb4364f7580716aff
Xillians <sandra.toftemo@bjerk.io>
2025-03-04 19:21:51 +0100

    adjust index content

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <h1>
-    App body
-  </h1>
-  <p>
-    This is the body of the app
-  </p>
+  <div>
+    <h1>
+      App body
+    </h1>
+    <p>
+      This is the body of the app
+    </p>
+  </div>
 </template>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+h1 {
+  align-self: center;
+}
+</style>
