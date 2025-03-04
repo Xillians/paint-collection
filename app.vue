1df46abf86a2072a696c45957aca4079f2ec3288
Xillians <sandra.toftemo@bjerk.io>
2025-03-04 17:15:04 +0100

    add auto-push to login when not logged in

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+ const { isLoggedIn } = useAuthState();
+ const router = useRouter();
+
+  if (!isLoggedIn.value && router.currentRoute.value.path !== "/login") {
+    router.push("/login");
+  }
+</script>
 <template>
   <main>
     <NuxtPage />
