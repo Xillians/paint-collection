848b5151bfabdd39cf37cadeb0ccdbb2d0ca1826
Xillians <sandra.toftemo@bjerk.io>
2025-03-04 14:50:38 +0100

    get typed input

--- server/api/login.ts
+++ server/api/login.ts
@@ -1,9 +1,17 @@
 import { readBody } from 'h3';
 
+type LoginBody = {
+  username: string;
+  password: string;
+};
+
 export default defineEventHandler(async (event) => {
-  const body = await readBody(event);
+  const body = await readBody<LoginBody>(event);
   console.log(JSON.stringify(body));
   return {
-    body: JSON.stringify(body)
+    body: {
+      username: body.username,
+      password: body.password
+    }
   }
 });
