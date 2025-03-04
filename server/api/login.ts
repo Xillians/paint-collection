0344ac0c6aadd0c6b5c637cd319f06d6c43c1651
Xillians <sandra.toftemo@bjerk.io>
2025-03-04 16:59:02 +0100

    add cookie to login

--- server/api/login.ts
+++ server/api/login.ts
@@ -1,4 +1,6 @@
 import { readBody } from 'h3';
+import { setCookie } from 'h3';
+import { useAuthState } from '~/composables/useAuthState';
 
 type LoginBody = {
   username: string;
@@ -6,12 +8,17 @@ type LoginBody = {
 };
 
 export default defineEventHandler(async (event) => {
+  const { isLoggedIn } = useAuthState();
   const body = await readBody<LoginBody>(event);
   console.log(JSON.stringify(body));
-  return {
-    body: {
-      username: body.username,
-      password: body.password
-    }
-  }
+  const userId = 4201;
+
+  setCookie(event, 'session', userId.toString(), {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+    path: '/',
+    maxAge: 60 * 60 * 24 * 7 // 1 week
+  });
+  isLoggedIn.value = true;
 });
