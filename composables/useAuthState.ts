0946ef8d14ed61513bea7ec981ee1cd205fd9137
Xillians <sandra.toftemo@bjerk.io>
2025-03-04 17:14:22 +0100

    use client session cookie

--- composables/useAuthState.ts
+++ composables/useAuthState.ts
@@ -1,7 +1,14 @@
 import { ref } from 'vue'
 
 export function useAuthState() {
+  const session = useCookie('clientSession');
   const isLoggedIn = ref(false);
+
+  if (session.value) {
+    isLoggedIn.value = true;
+  }
+
+  console.log('isLoggedIn', isLoggedIn.value);
 
   return { isLoggedIn }
 }
