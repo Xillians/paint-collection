df58a083851c344dc94aafa5e34143619c969949
Xillians <sandra.toftemo@bjerk.io>
2025-03-04 15:38:15 +0100

    add correct base url

--- composables/paint-api.ts
+++ composables/paint-api.ts
@@ -1,8 +1,9 @@
 import { PaintAPI, type OpenAPIConfig } from "~/server/utils/openapi";
 
+
 function usePaintApi(): PaintAPI {
   const apiConfig: Partial<OpenAPIConfig> = {
-    BASE: 'https://api.example.com',
+    BASE: 'https://paint-api-v2.fly.dev',
     VERSION: '0.1.0',
   }
   const paintApi = new PaintAPI(apiConfig);
