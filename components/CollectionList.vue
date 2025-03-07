84829a7848ed0dd1231eed61a87674bcaed7a903
Xillians <sandra.toftemo@bjerk.io>
2025-03-07 10:49:56 +0100

    use entry card component instead of direct implementation

--- components/CollectionList.vue
+++ components/CollectionList.vue
@@ -1,43 +1,11 @@
 <template>
   <div class="entry-list">
-    <button v-for="collection in collectionList" :key="collection.id" class="entry-card">
-      <LabelledField>
-        <p>{{ collection.quantity }}x</p>
-      </LabelledField>
-      <LabelledField label="Brand">
-        <p>{{ collection.paint.brand.name }}</p>
-      </LabelledField>
-      <LabelledField label="Name">
-        <p>{{ collection.paint.name }}</p>
-      </LabelledField>
-      <LabelledField>
-        <div :style="{ 
-          backgroundColor: collection.paint.color_code,
-          height: '20px',
-          width: '20px',
-        }"></div>
-      </LabelledField>
-    </button>
+    <EntryCard />
     <button aria-label="add entry">
       <h2>+</h2>
     </button>
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue';
-import type { CollectionPaintDetails } from '~/server/utils/openapi';
-  const collectionList = ref<CollectionPaintDetails[]>([]);
-
-  onMounted(() => {
-    fetchCollectionList();
-  });
-
-  async function fetchCollectionList() {
-    const response = await $fetch('/api/listCollection');
-    collectionList.value = response;
-  }
-</script>
 
 <style scoped>
 h2 {
@@ -45,12 +13,8 @@ h2 {
 }
 button {
   background-color: transparent;
-  border-radius: 0;
-  border: none;
-  color: var(--sunglo);
   height: fit-content;
   cursor: pointer;
-  transition: background-color 0.3s ease-out, border-radius 0.3s ease-out;
 }
 button:hover,
 button:focus {
@@ -59,6 +23,7 @@ button:focus {
   border-radius: 5px;
   transition: background-color 0.3s ease-out, border-radius 0.3s ease-out;
 }
+
 .entry-list {
   display: flex;
   flex-direction: column;
@@ -67,14 +32,5 @@ button:focus {
   height: 100%;
   width: 80%;
   margin: 0;
-}
-.entry-card {
-  display: grid;
-  grid-template-columns: 1fr 4fr 4fr 1fr auto;
-  align-items: center;
-  width: 100%;
-  gap: 1rem;
-  border-bottom: 1px solid #ccc;
-  height: fit-content;
 }
 </style>
