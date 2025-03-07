a7c299a389eeed013d816dc9abd0e03428d5bd06
Xillians <sandra.toftemo@bjerk.io>
2025-03-07 09:44:27 +0100

    use parsed logic on login

--- server/api/login.ts
+++ server/api/login.ts
@@ -1,7 +1,7 @@
 import { readBody } from 'h3';
 import { setCookie } from 'h3';
 import { usePaintApi } from '~/composables/paintApi';
-import { ErrorModel } from '../utils/openapi';
+import { LoginOutputBody } from '../utils/openapi';
 import { differenceInSeconds, parseISO } from 'date-fns';
 
 type LoginBody = {
@@ -14,27 +14,22 @@ export default defineEventHandler(async (event) => {
   const cookies = parseCookies(event);
   const token = cookies.session;
   console.log(JSON.stringify(body));
-  const { paintApi } = usePaintApi(token);
+  const { paintApi, parseApiResponse, parseError } = usePaintApi(token);
 
   try {
     const userId = 4201;
     const response = await paintApi.users.getLogin(userId);
-    if (!('token' in response)) {
-      response satisfies ErrorModel;
-      throw createError({
-        status: response.status,
-        message: response.detail
-      })
-    }
+    const parsedResponse = parseApiResponse<LoginOutputBody>(response);
+    const { token, expires_at } = parsedResponse;
 
     // A hack with date-fns and non-standard date format
-    const expiresAtString = response.expires_at.split(' ')[0] + 'T' + response.expires_at.split(' ')[1];
+    const expiresAtString = expires_at.split(' ')[0] + 'T' + expires_at.split(' ')[1];
 
     const expiresAt = parseISO(expiresAtString);
     const now = new Date();
     const maxAge = differenceInSeconds(expiresAt, now);
 
-    setCookie(event, 'session', response.token, {
+    setCookie(event, 'session', token, {
       httpOnly: true,
       secure: true,
       sameSite: 'strict',
@@ -43,20 +38,21 @@ export default defineEventHandler(async (event) => {
     });
 
     // set a cookie for client to read
-    setCookie(event, 'clientSession', response.token, {
+    setCookie(event, 'clientSession', token, {
       httpOnly: false,
       secure: false,
       sameSite: 'strict',
       maxAge: maxAge
     });
   } catch (error: any) {
-    if ("body" in error) {
-      const err = error.body as ErrorModel;
+    const apiError = parseError(error);
+    if (apiError) {
       throw createError({
-        status: err.status,
-        message: err.detail
+        status: apiError.status,
+        message: apiError.detail
       });
     }
+    console.log(error);
     throw createError({
       status: 500,
       message: 'Internal Server Error'
