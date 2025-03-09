463c25cbc7e80c99acc329a48672244f3f11c2a6
Xillians <sandra.toftemo@bjerk.io>
2025-03-09 12:31:41 +0100

    add list brands API path

---
+++ server/api/listBrands.ts
@@ -0,0 +1,25 @@
+import { usePaintApi } from "~/composables/paintApi";
+import { ListBrandOutputBody } from "../utils/openapi";
+
+export default defineEventHandler(async (event) => {
+  const session = parseCookies(event).session;
+  const { paintApi, parseApiResponse, parseError } = usePaintApi(session);
+
+  try {
+    const response = await paintApi.paintBrands.getPaintBrands()
+    const parsedResponse = parseApiResponse<ListBrandOutputBody>(response);
+    return parsedResponse.brands;
+  } catch (error: any) {
+    const err = parseError(error);
+    if (err) {
+      throw createError({
+        status: err.status,
+        message: err.detail
+      });
+    }
+    throw createError({
+      status: 500,
+      message: error.message
+    });
+  }
+});
