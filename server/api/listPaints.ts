1cfde7be7368d3a0d4567b6d739ea0cf0dcf2058
Xillians <sandra.toftemo@bjerk.io>
2025-03-09 22:18:52 +0100

    add list paint server api

---
+++ server/api/listPaints.ts
@@ -0,0 +1,25 @@
+import { usePaintApi } from "~/composables/paintApi";
+import { ListPaintOutputBody } from "../utils/openapi";
+
+export default defineEventHandler(async (event) => {
+  const session = parseCookies(event).session;
+  const { paintApi, parseApiResponse, parseError } = usePaintApi(session);
+
+  try {
+    const response = await paintApi.paints.getPaints();
+    const parsedResponse = parseApiResponse<ListPaintOutputBody>(response);
+    return parsedResponse.paints;
+  } catch (error: any) {
+    const err = parseError(error);
+    if (err) {
+      throw createError({
+        status: err.status,
+        message: err.detail
+      });
+    }
+    throw createError({
+      status: 500,
+      message: error.message
+    });
+  }
+});
