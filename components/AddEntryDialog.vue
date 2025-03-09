650e594e28670d4bda3f3b8b23bb9f1673d890bd
Xillians <sandra.toftemo@bjerk.io>
2025-03-09 22:19:48 +0100

    make choose color own component

--- components/AddEntryDialog.vue
+++ components/AddEntryDialog.vue
@@ -7,27 +7,19 @@
       Add Entry
     </template>
     <form @submit.prevent="addEntry">
-      <label for="title">Title</label>
-      <input v-model="title" type="text" placeholder="Title" required />
       <label for="amount">Amount</label>
       <input v-model="amount" type="number" placeholder="Amount" required />
-      <label for="brand">Brand</label>
-      <select v-model="chosenBrand">
-        <option v-for="brand in brands" :key="brand.id" :value="brand.id">
-          {{ brand.name }}
-        </option>
-      </select>
-      <button type="submit">OK</button>
+      <ChooseColor />
+      <button type="submit">Add entry</button>
     </form>
   </BaseDialog>
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
-import { type PaintBrands } from '~/server/utils/openapi';
+import { ref } from 'vue';
 import BaseDialog from './base/BaseDialog.vue';
 
-const { brands, title, amount, chosenBrand } = useEntryForm();
+const { amount } = useEntryForm();
 const dialog = ref<InstanceType<typeof BaseDialog> | null>(null);
 
 function openDialog() {
@@ -35,27 +27,13 @@ function openDialog() {
 }
 
 function addEntry() {
-  console.log("Adding entry");
-  console.log({ title: title.value, amount: amount.value, brand: chosenBrand.value });
   dialog.value?.closeDialog(new Event('close'));
 }
 
-onMounted(async () => {
-  try {
-    const response = await fetch('/api/listBrands');
-    brands.value = await response.json();
-  } catch (error) {
-    brands.value = [];
-  }
-});
-
 function useEntryForm() {
-  const brands = ref<PaintBrands[]>([]);
-  const title = ref('');
   const amount = ref('');
-  const chosenBrand = ref('');
 
-  return { brands, title, amount, chosenBrand };
+  return { amount };
 }
 </script>
 
