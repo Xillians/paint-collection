29b545c3d271e782c21c3dc8a07eeea0117e00ce
Xillians <sandra.toftemo@bjerk.io>
2025-03-10 10:40:57 +0100

    use dialog component

--- components/EntryCard.vue
+++ components/EntryCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="() => openDialog(collection.id)" v-for="collection in collection" :key="collection.id" class="entry-card transparent">
+  <button @click="() => dialog?.openDialog(collection.id)" v-for="collection in collection" :key="collection.id" class="entry-card transparent">
     <LabelledField>
       <p>{{ collection.quantity }}x</p>
     </LabelledField>
@@ -17,54 +17,15 @@
       }"></div>
     </LabelledField>
   </button>
-  <BaseDialog ref="dialog">
-    <template #title>
-      Update color
-    </template>
-    <form>
-      <label for="amount">Amount</label>
-      <input v-model="amount" type="number" placeholder="Amount" required />
-      <ChooseColor @update:chosenPaint="handleChosenPaint" />
-      <div class="buttons">
-        <button aria-label="update entry" type="submit">Update</button>
-        <button aria-label="delete entry" @click="handleDelete">Delete</button>
-      </div>
-    </form>
-  </BaseDialog>
+  <UpdateEntryDialog ref="dialog" />
 </template>
 
 <script setup lang="ts">
-  import type { BaseDialog } from '#components';
+  import type { UpdateEntryDialog } from '#components';
   import { ref } from 'vue';
-  import type { PaintOutputDetails } from '~/server/utils/openapi';
 
   const { collection } = useColorState;
-
-  const amount = ref<number | null>(null);
-  const color = ref<PaintOutputDetails | null>(null);
-  const dialog = ref<InstanceType<typeof BaseDialog> | null>(null);
-  const currentCollectionId = ref<number | null>(null);
-
-  function openDialog(id: number) {
-    currentCollectionId.value = id;
-    dialog.value?.openDialog();
-  }
-  function handleChosenPaint(chosenPaint: PaintOutputDetails) {
-    color.value = chosenPaint;
-  }
-  async function handleDelete(event: Event) {
-    if (!currentCollectionId.value) return;
-    try {
-      await $fetch(`/api/collection/${currentCollectionId.value}/deleteEntry`, {
-        method: 'DELETE',
-      });
-      const response = await $fetch('/api/collection/listCollection');
-      collection.value = response;
-      dialog.value?.closeDialog(event);
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  const dialog = ref<InstanceType<typeof UpdateEntryDialog> | null>(null);
 
   onMounted(async () => {
     const reponse = await $fetch('/api/collection/listCollection');
@@ -73,12 +34,6 @@
 </script>
 
 <style scoped>
-form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  gap: 1rem;
-}
 .entry-card {
   display: grid;
   grid-template-columns: 1fr 4fr 4fr 1fr auto;
@@ -87,9 +42,5 @@ form {
   gap: 1rem;
   border-bottom: 1px solid var(--button-background);
   height: fit-content;
-}
-.buttons {
-  display: flex;
-  gap: 1rem;
 }
 </style>
