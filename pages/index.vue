3a36cb0088eab6a790eb8f6e821944740e93289e
Xillians <sandra.toftemo@bjerk.io>
2025-03-20 16:25:05 +0100

    Delete unnecessary layered component

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <CollectionList />
+  <div class="entry-list">
+    <EntryCard />
+    <AddEntryDialog />
+  </div>
 </template>
+
+<style scoped>
+.entry-list {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+  height: 100%;
+  width: 80%;
+}
+</style>
