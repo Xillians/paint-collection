5d7307f10bbc1667ae7cecd49ae69f4904f368b2
Xillians <sandra.toftemo@bjerk.io>
2025-04-09 18:39:17 +0200

    add coverage and globals

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,15 @@ export default defineVitestConfig({
   test: {
     environment: 'nuxt',
     passWithNoTests: true,
+    globals: true,
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        '.nuxt/',
+        'utils/openapi/',
+      ],
+    },
     exclude: [
       '**/node_modules/**',
       '**/.nuxt/**',
